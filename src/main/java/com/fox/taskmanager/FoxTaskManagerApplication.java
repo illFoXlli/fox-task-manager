06d543b9c99647b988ee16a3d06b66ccd22f148b
illFoXlli <fox.ruden@egmail.com>
2026-05-19 15:37:32 +0300

    initial spring boot project setup

---
+++ src/main/java/com/fox/taskmanager/FoxTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fox.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxTaskManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fox/taskmanager/FoxTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fox.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
